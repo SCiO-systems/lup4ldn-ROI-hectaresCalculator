6df36cd088ad0ce7fbe98c14ad5ecbac50c456bf
Christos Charisis <christos@scio.systems>
2022-05-09 12:03:29 +0100

    cleaned code for public repo

--- ROI_hectares_calcualtor.py
+++ ROI_hectares_calcualtor.py
@@ -3,14 +3,10 @@ import numpy as np
 import os
 import sys
 import requests
-# import numpy.ma as ma
-# import copy
 import json
 import boto3
 from botocore.exceptions import ClientError
-# import geopandas as gpd
 import logging
-
 
 s3 = boto3.client('s3')
 #%%
@@ -23,20 +19,29 @@ def lambda_handler(event, context):
     try:
         project_id = json_file["project_id"]
         pol_shape = json_file["polygon"]
+        if pol_shape==None:
+            pol_shape = requests.get(json_file["polygon_url"])
+            pol_shape = json.loads(pol_shape.text) #.replace("'",'"')
+            json_file["polygon"] = pol_shape
+            
         roi_shape = json_file["ROI"]
+        if roi_shape==None:
+            roi_shape = requests.get(json_file["ROI_file_url"])
+            roi_shape = json.loads(roi_shape.text) #.replace("'",'"')
+            json_file["ROI"] = roi_shape
+            
     except Exception as e:
-        print(e)
         print("Input JSON field have an error.")
+        return {
+            "statusCode": 400,
+            "body": e
+        }
     
-
-    #for local
-    # path_to_tmp = "/home/christos/Desktop/SCiO_Projects/lup4ldn/data/cropped_files/"
+    
     #for aws
     path_to_tmp = "/tmp/"
-    
-    # s3_file_path = '/vsis3/lup4ldn-dataset' + "/" + country_iso + "/"
-    # s3_file_path = "https://lup4ldn-default-global-datasets.s3.eu-central-1.amazonaws.com/"
     s3_file_path = '/vsis3/lup4ldn-default-global-datasets/'
+    
     path_to_100m_grid = s3_file_path + "global_100m_grid.tif"
     save_intersection_path = path_to_tmp + "intersection.tif"
     
@@ -67,15 +72,18 @@ def lambda_handler(event, context):
         try:
             gdal.Warp(save_intersection_path,path_to_100m_grid,**gdal_warp_kwargs_target_area)
         except Exception as e:
-            print(e)
             print("if 'returned NULL without setting an error', probably at least one of the file paths is wrong")
+            raise(e)
         
         try:
             intersect_area_tif = gdal.Open(save_intersection_path)
             intersect_area_array = intersect_area_tif.ReadAsArray()
         except Exception as e:
-            print(e)
             print("if ''NoneType' object has no attribute', probably the file path is wrong")
+            return {
+                "statusCode": 400,
+                "body": e
+            }
         
         intersect_area_array = np.where(intersect_area_array!=-32768,1,-32768)
         
